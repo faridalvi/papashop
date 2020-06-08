524efe14781a2a543c221d33a65ab6441e116eb5
naumantuf <nauman.farooq@tuf.edu.pk>
2020-06-08 11:55:26 +0500

    Products view updated

--- resources/views/front/products.blade.php
+++ resources/views/front/products.blade.php
@@ -4,13 +4,26 @@
     <div class="container mt-4">
         <div class="row">
             @foreach($marts as $mart)
+                @if(count($mart->products) > 0)
                 <div class="col-md-3 col-sm-4">
-                    @foreach($mart->products as $product)
-                        {{$product->name}}
-                    @endforeach
+                        @foreach($mart->products as $product)
+                            <div class="card">
+                                <img class="card-img-top" src="{{asset('public/storage/image/'.$product->image)}}" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{$product->name}}</h5>
+                                    <p class="card-text">
+                                        {{$product->description}}
+                                    </p>
+                                    <a href="#" class="btn btn-primary btn-sm">Buy Now</a>
+                                </div>
+                            </div>
+                        @endforeach
                 </div>
-            @endforeach
+                @else
+                    <h3>{{$mart->name}} has no products</h3>
+                @endif
 
+            @endforeach
         </div>
     </div>
 @endsection
